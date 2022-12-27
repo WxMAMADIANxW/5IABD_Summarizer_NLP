759f48b1e6da1e1d90f94da0397098f817150ffa
Réda Maizate <reda.maizate@affluences.com>
2022-12-28 00:36:17 +0100

    feat(back): Added back part

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM python:3.8.1-slim
-
-COPY app /app
-WORKDIR /app
-
-COPY requirements.txt requirements.txt
-RUN pip install -r requirements.txt
-
-CMD ["python", "front.py"]
--- app/back/main.py
+++ app/back/main.py
@@ -1,3 +1,4 @@
+import os
 import youtube_dl
 from pytube import YouTube as yt
 import whisper
@@ -6,31 +7,51 @@ from transformers import pipeline
 from docx import Document
 from fpdf import FPDF
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+import uvicorn
+import boto3
+
+app = FastAPI()
 
 
-
+@app.get("/summarize")
+def summarize(link: str):
+    audio_path = download_youtube_video_to_mp3(link)
+    text = whisper_process(audio_path)
+    summary = hugging_face(text)
+    txt_to_pdf(summary)
+    return JSONResponse(summary)
 
 
 ##Pytube
-
-def py_tube(link):
+def download_youtube_video_to_mp3(link: str):
     audio = yt(link).streams.filter(only_audio=True).all()
-    return audio[0].download("/content/mp3")
+    audio[0].download(f"/tmp/audio-{link}.mp3")
+    # Download to S3 bucket
+    s3 = boto3.client('s3')
+    try:
+        s3.upload_file(f"/tmp/audio-{link}.mp3", "video-summarizer-bucket", f"audio-{link}.mp3")
+    except Exception as e:
+        raise e
 
+    return f"audio-{link}.mp3"
 
 
 ##Whisper
-
-def whisper_process(filename):
+def whisper_process(object_name):
     WhispBase = whisper.load_model('base')
-    Text = WhispBase.transcribe(filename,  fp16=False)
-    return Text['text']
-
+    s3 = boto3.client('s3')
+    path = f"/tmp/{object_name}"
+    try:
+        s3.meta.client.download_file("video-summarizer-bucket", object_name, path)
+    except Exception as e:
+        raise e
+    text = WhispBase.transcribe(path, fp16=False)
+    return text['text']
 
 
 ##HuggingFace
-
-def hugging_face(Text_to_summarize):
+def hugging_face(text_to_summarize):
     summarizer = pipeline(
         "summarization",
         "pszemraj/long-t5-tglobal-base-16384-book-summary",
@@ -48,14 +69,11 @@ def hugging_face(Text_to_summarize):
         "num_beams": 4,
     }
 
-    result = summarizer(Text_to_summarize, **params)
+    result = summarizer(text_to_summarize, **params)
     return result[0]["summary_text"]
 
 
-
-
 ##FPDF
-
 def txt_to_pdf(text_to_sum):
     pdf = FPDF()
     pdf.set_font("Arial", size=15)
@@ -66,4 +84,6 @@ def txt_to_pdf(text_to_sum):
 
     pdf.output("resume.pdf")
 
+
 if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=5000)

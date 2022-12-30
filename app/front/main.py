2b1dcc8fc5db151ee2f45877da1110fb83a67a32
Réda Maizate <reda.maizate@affluences.com>
2022-12-30 23:23:50 +0100

    feat(front): Added port in the url directly v2

--- app/front/main.py
+++ app/front/main.py
@@ -15,7 +15,7 @@ if button:
     # When the button is clicked, download the video from the link
     streamlit.write("Downloading video...")
     # Call to EC2 instance
-    response = requests.get(f"http://{BACKEND_URL}:5g000/summarize?"
+    response = requests.get(f"http://{BACKEND_URL}:5000/summarize?"
                             f"link={link}")
     # Display the summary
     streamlit.write("The summary of the video : \n", response.text)
